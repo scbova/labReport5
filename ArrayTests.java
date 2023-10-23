55d74483097d4d030358f08e0cfd9be1bb559638
scbova <sbova@ucsd.edu>
2023-10-23 14:40:26 -0700

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,17 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void longReverseInPlace() {
+    int[] input1 = { 3, 4, 5 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 5, 4, 3 }, input1);
+  }
+
+  @Test
+  public void longReversed() {
+    int[] input1 = { 4, 5, 6 };
+    assertArrayEquals(new int[]{ 6, 5, 4 }, ArrayExamples.reversed(input1));
+  }
 }
