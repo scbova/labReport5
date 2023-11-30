65d5e18ba9e4b76f258143770eb8a3358b618c37
scbova <sbova@ucsd.edu>
2023-11-30 12:01:09 -0800

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,27 +2,10 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-  @Test
-  public void testSort() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.sort(input1));
-  }
 
   @Test
   public void testSort1() {
     int[] input1 = { 3, 4, 5};
-    assertArrayEquals(new int[]{ 3, 4, 5 }, ArrayExamples.sort(input1));
-  }
-
-  @Test
-  public void testSort2() {
-    int[] input1 = { 5, 4, 3};
-    assertArrayEquals(new int[]{ 3, 4, 5 }, ArrayExamples.sort(input1));
-  }
-
- @Test
-  public void testSort3() {
-    int[] input1 = { 5, 3, 4};
-    assertArrayEquals(new int[]{ 3, 4, 5 }, ArrayExamples.sort(input1));
+    assertArrayEquals(new int[]{ 3, 4, 5 }, ArraySort.sort(input1));
   }
 }
